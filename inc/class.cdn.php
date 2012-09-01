6f9032f5d9cf6fad179ea8e9ba29e0d98fcd38aa
Marko Heijnen <info@markoheijnen.com>
2012-09-01 19:28:56 +0200

    Changed CDN logic

--- inc/class.cdn.php
+++ inc/class.cdn.php
@@ -5,22 +5,22 @@ class Rockstar_Speed_Cdn {
 	private $cdn_url;
 
 	function __construct() {
-		add_action( 'init', array( &$this, 'set_site_url' ) );
-
-		add_action( 'template_redirect', array( &$this, 'activate_cdn' ), -1 );
+		add_action( 'init', array( &$this, 'set_urls' ) );
 	}
 
 	function set_site_url() {
 		$this->site_url = preg_replace( "((https?)://)", "", site_url() );
 		$this->cdn_url  = apply_filters( 'rockstarspeed_cdn_url', false );
+
+		if( ! $this->cdn_url ) {
+			add_action( 'template_redirect', array( &$this, 'activate_cdn' ) );
+		}
 	}
 
 	function activate_cdn() {
-		if( $this->cdn_url ) {
-			add_filter( 'script_loader_src', array( &$this, 'filter_js' ) );
-			add_filter( 'style_loader_src', array( &$this, 'filter_css' ) );
-			add_filter( 'theme_root_uri', array( &$this, 'filter_theme' ) );
-		}
+		add_filter( 'script_loader_src', array( &$this, 'filter_js' ) );
+		add_filter( 'style_loader_src', array( &$this, 'filter_css' ) );
+		add_filter( 'theme_root_uri', array( &$this, 'filter_theme' ) );
 	}
 
 	function filter_js( $url ) {
